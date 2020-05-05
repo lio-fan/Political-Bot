bc114bf1c2a40ab2a81b6f6701e114344515c106
lio-fan <liofanin2019@Gmail.com>
2020-05-05 21:48:59 +0000

    2nd commit on LeaveParty command. You can now leave your designated party.

--- commands/leave.js
+++ commands/leave.js
@@ -8,15 +8,39 @@ class LeaveCommand extends Command {
  }
 
  exec(message) {
+  let republicanRole = message.guild.roles.cache.find(role => role.name === "Republican");
+  let democraticRole = message.guild.roles.cache.find(role => role.name === "Democrat");
+
    if (message.member.roles.cache.find(r => r.name === "Democrat")) {
 
     const embed = new Discord.MessageEmbed()
      .setColor(color)
-     .setDescription(`**${message.author.tag}**, you are already part of the **Democrat** party.`)
-
+     .setDescription(`**${message.author.tag}**, you have successfully left the **Democratic** party.`)
+		
+		message.member.roles.remove(democraticRole)
 
     return message.channel.send(embed);
    }
+
+   if (message.member.roles.cache.find(r => r.name === "Republican")) {
+
+    const embed = new Discord.MessageEmbed()
+     .setColor(color)
+     .setDescription(`**${message.author.tag}**, you have successfully left the **Republican** party.`)
+		
+		message.member.roles.remove(republicanRole)
+
+    return message.channel.send(embed);
+   }
+
+
+    const embed = new Discord.MessageEmbed()
+     .setColor(color)
+     .setDescription(`**${message.author.tag}**, you aren't part of a party.`)
+		
+    return message.channel.send(embed);
+
+
  }
 }
 
