3bf8e4fd516f84be5e810c8a5f5546c6a5500d15
lio-fan <47541998+lio-fan@users.noreply.github.com>
2020-05-15 14:03:36 -0700

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -10,7 +10,7 @@ class LeaveCommand extends Command {
  exec(message) {
 
 
- const config = require('/home/runner/Political-Bot/config.json')
+ const config = require('/app/config.json')
   let colors = config.colors
   let color = colors[Math.floor(Math.random() * colors.length)];	 
   let republicanRole = message.guild.roles.cache.find(role => role.name === "Republican");
